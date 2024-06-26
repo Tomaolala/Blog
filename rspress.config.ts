d54a8deee1cad3c3fcc3b99faa23df6d8baa3bac
Tomao <ziyuhang@wepie.com>
2024-06-26 20:05:24 +0800

    fix: add plugins and components

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,10 +1,11 @@
 
 import { defineConfig } from 'rspress/config';
-
+import { pluginPreview } from '@rspress/plugin-preview';
+import readingtime from 'rspress-plugin-reading-time'
 export default defineConfig({
   // 文档根目录
   root: 'docs',
-  base:'/Blog',
+  // base:'/Blog',
   icon:'https://lf3-static.bytednsdoc.com/obj/eden-cn/rjhwzy/ljhwZthlaukjlkulzlp/rspress/rspress-navbar-logo-0904.png',
   logo: {
     light:
@@ -12,6 +13,7 @@ export default defineConfig({
     dark: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/rjhwzy/ljhwZthlaukjlkulzlp/rspress/rspress-navbar-logo-dark-0904.png',
   },
   themeConfig: {
+    lastUpdated:true,
     enableContentAnimation: true,
     footer: {
       message: 'From Yuhang Zi       Happy for living',
@@ -28,8 +30,11 @@ export default defineConfig({
         mode: 'link',
         content: 'https://github.com/Tomaolala/Blog',
       },
-    ]}
-
+    ]},
+    markdown: {
+      showLineNumbers: true,
+    },
+    plugins:[pluginPreview(),readingtime()]
 });
 
 
